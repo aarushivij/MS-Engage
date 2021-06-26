93203659f5277b3059fa5c61eea35e1f434335ed
aarushivij <aarushivij@gmail.com>
2021-06-26 22:58:48 +0530

    add_info_icon

--- msEngage/app/src/main/java/com/example/msengage/activities/MainActivity.java
+++ msEngage/app/src/main/java/com/example/msengage/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.msengage.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
